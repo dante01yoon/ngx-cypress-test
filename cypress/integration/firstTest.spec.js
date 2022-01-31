b87cb349c118e103cb9db1b7928b34c3d0fdeac7
jaewon-yoon <jaewon.yoon@navercorp.com>
2022-01-31 21:58:38 +0900

    Update: table

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -201,7 +201,7 @@ describe("Our First suite", () => {
     cy.get('[type="checkbox"]').eq(1).check({force: true})
   })
 
-  it.only('lists and dropdowns', () => {
+  it.skip('lists and dropdowns', () => {
     cy.visit('/');
     // 1
     // cy.get('nav nb-select').click();
@@ -233,7 +233,55 @@ describe("Our First suite", () => {
             }
           })
       })
+    // end of it
   })
+
+  it.only("Web tables", () => {
+    cy.visit('/');
+    cy.contains('Tables & Data').click();
+    cy.contains('Smart Table').click();
+  
+    // 1
+    // cy.get('tbody')
+    //   .contains('tr', 'Larry')
+    //   .then(tableRow => {
+    //     cy.wrap(tableRow).find('.nb-edit').click()
+    //     cy.wrap(tableRow).find('[placeholder="Age"]').type('25');
+    //     cy.wrap(tableRow).find('.nb-checkmark').click()
+    //     cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+    //   });
+    
+    // 2
+    // cy.get('thead').find('.nb-plus').click();
+    // cy.get('thead').find('tr').eq(2).then(tableRow => {
+    //   cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
+    //   cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+    //   cy.wrap(tableRow).find('.nb-checkmark').click();
+    // })
+    // cy.get('tbody tr').first().find('td').then(tableColumns => {
+    //   cy.wrap(tableColumns).eq(2).should('contain', 'Artem');
+    //   cy.wrap(tableColumns).eq(3).should('contain', 'Bondar');
+    // })
+
+    // 3
+    const age = [20, 30, 40, 200];
+
+    cy.wrap(age).each( age => {
+      cy.get('thead [placeholder="Age"]').clear().type(age);
+      cy.wait(500)
+      cy.get('tbody tr')
+        .each((tableRow) => {
+          if(age === 200) {
+            cy.wrap(tableRow).should('contain', 'No data found')
+          } else {
+            cy.wrap(tableRow).find('td').eq(6).should('contain', age);
+          }
+        })
+    })
+
+    
+    // end of it
+  });
 
   // end of describe
 })
