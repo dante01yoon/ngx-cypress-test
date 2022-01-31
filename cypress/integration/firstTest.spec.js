7be6245a9648d0458294960977592438d9f216bb
jaewon-yoon <jaewon.yoon@navercorp.com>
2022-01-31 20:56:19 +0900

    Update: checkbox

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -148,7 +148,7 @@ describe("Our First suite", () => {
       // end of it
   });
 
-  it.only('assert property', () => {
+  it.skip('assert property', () => {
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
@@ -162,6 +162,44 @@ describe("Our First suite", () => {
       })
 
   });
+
+  it.skip('radio button', () => {
+    cy.visit("/");
+    cy.contains('Forms').click();
+    cy.contains('Form Layout').click();
+
+    cy.contains('nb-card', 'Using the Grid')
+      .find('[type="radio"]')
+      .then(radioButtons => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({force: true})
+          .should('be.checked')
+
+        cy.wrap(radioButtons)
+          .eq(1)
+          .check({ force: true})
+        
+        cy.wrap(radioButtons)
+          .first()
+          .should('not.be.checked')
+        
+        cy.wrap(radioButtons)
+          .eq(2)
+          .should('be.disabled')
+      })
+    // end of it
+  })
+
+  it.only('check boxes', () => {
+    cy.visit('/');
+    cy.contains('Modal & Overlays').click();
+    cy.contains('Toastr').click();
+
+    // cy.get('[type="checkbox"]').check({force: true});
+    cy.get('[type="checkbox"]').eq(0).click({force: true})
+    cy.get('[type="checkbox"]').eq(1).check({force: true})
+  })
 
   // end of describe
 })
