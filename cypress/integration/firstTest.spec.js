c7a387d72519e309b6d26fab707236b7f139b7d6
jaewon-yoon <jaewon.yoon@navercorp.com>
2022-01-31 19:18:59 +0900

    Update: Saving Subject

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -5,7 +5,7 @@ const { CtrCompleter } = require("ng2-completer")
 
 describe("Our First suite", () => {
   
-  it("first test", () => {
+  it.skip("first test", () => {
     cy.visit('/')
     cy.contains("Forms").click()
     cy.contains("Form Layouts").click()
@@ -41,11 +41,76 @@ describe("Our First suite", () => {
     cy.get('[data-cy="imputEmail"]')
   })
 
-  it("second test", () => {
-    
+  it.skip("second test", () => {
+    cy.visit('/')
+    cy.contains("Forms").click()
+    cy.contains("Form Layouts").click()
+
+    cy.get('[data-cy="signInButton"]');
+
+    cy.contains('Sign in');
+
+    cy.contains("[status='warning']","Sign in")
+
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+    cy.contains('nb-card','Horizontal form').find('[type="email"]')
+
   })
 
-  it("third test", () => {
+  it.only("then and wrap methods", () => {
+    cy.visit('/')
+    cy.contains("Forms").click()
+    cy.contains("Form Layouts").click()
+
+    cy.contains('nb-card', "Using the Grid")
+      .find('[for="inputEmail1"]')
+      .should('contain', 'Email')
+
+    cy.contains('nb-card', 'Using the Grid')
+      .find('[for="inputPassword2"]')
+      .should('contain', "Password")
+
+    cy.contains('nb-card', 'Basic form')
+    .find('[for="exampleInputEmail1"]')
+    .should('contain', "Email address")
     
+    cy.contains('nb-card', 'Basic form')
+    .find('[for="exampleInputPassword1"]')
+    .should('contain', "Password")
+
+    // selenium style - not works like this
+    // const firstForm = cy.contains('nb-card', "Using the Grid");
+    // const secondForm = cy.contains('nb-card', "Basic form");
+
+    // firstForm.find('[for="inputEmail1"]').should('contain', 'Email');
+    // firstForm.find('[for="inputPassword2"]').should('contain', 'Password');
+    // secondForm.find('[for="exampleInputEmail1"]').should('contain', 'Email address');
+
+    // cypress style
+    cy.contains('nb-card', 'Using the Grid')
+      .then((firstForm) => { // firstForm is jQuery Object
+        const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text();
+        const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text();
+        expect(emailLabelFirst).to.equal('Email');
+        expect(passwordLabelFirst).to.equal("Password");
+
+        cy.contains('nb-card', 'Basic form')
+          .then(secondForm => {
+            const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text();
+            expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+            cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', "Password")
+          })
+      })
+
+    // end of it
   })
+  // end of describe
 })
